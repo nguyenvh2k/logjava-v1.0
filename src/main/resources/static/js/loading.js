d394cdf9ec9b2b6b146d07c6b7536c525df1d289
Smile <nguyenvh2k@gmail.com>
2023-03-25 15:36:51 +0700

    add loader page

---
+++ src/main/resources/static/js/loading.js
@@ -0,0 +1,9 @@
+$(window).load(function () {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
+
+// $(".se-pre-con").show();
+// $(document).ready(function () {
+//      $(".se-pre-con").hide();
+// });
